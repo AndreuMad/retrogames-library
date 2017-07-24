b06abc51e8119f9a2cfe7f5f7172516f06ae4f50
Andrii Kit <andrii.labyk@gmail.com>
2017-07-24 23:10:34 +0300

    1

--- app/routes/game.js
+++ app/routes/game.js
@@ -2,7 +2,57 @@
 import Game from '../models/game';
 
 // get all the games sorted by postDate
-const getgames = (req, res) => {
+const getGames = (req, res) => {
 
     // Query the DB, if no errors send all the games to the client
-}
+    Game.find(null, null, { sort: { postDate: 1 } }, (err, games) => {
+        if(err) {
+            res.send(err)
+        }
+        res.json(games);
+    });
+};
+
+// Get a single game filtered by ID
+const getGame = (req, res) => {
+    const { id } = req.params;
+    // Query the DB for a single game, if no errors send it to the client
+    Game.findById(id, (err, game) => {
+        if(err) {
+            res.send(err);
+        }
+        res.json(game); // Game sent as json
+    });
+};
+
+// Get the body data and create a new Game
+const postGame = (req, res) => {
+    // We assign the game info to a empty game and send a message back if no errors
+    let game = Object.assign(new Game(), req.body);
+    // ...Then we save it into the DB
+    game.save(err => {
+        if(err) {
+            res.send(err);
+        }
+        res.json({ message: 'game created' }); // A simple JSON answer to inform the client
+    });
+};
+
+// Delete a game by the given ID
+
+const deleteGame = (req, res) => {
+    //We remove the game by the given id and send a message back if no errors
+    Game.remove(
+        { _id: req.params.id},
+        err => {
+            if(err) {
+                res.send(err);
+            }
+            res.json({ message: 'successfully deleted' }); // A simple JSON answer to inform the client
+        }
+    );
+};
+
+// We export our functions to be used in the server routes
+
+export { getGames, getGame, postGame, deleteGame};
